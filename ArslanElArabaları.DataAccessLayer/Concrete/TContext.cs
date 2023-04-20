4517e904c54ff92d4fb9c5c89b316811878ed281
Enes Fevzi <118919666+EnesFevzi@users.noreply.github.com>
2023-04-20 03:55:28 +0300

    Update TContext.cs

--- ArslanElArabaları.DataAccessLayer/Concrete/TContext.cs
+++ ArslanElArabaları.DataAccessLayer/Concrete/TContext.cs
@@ -12,28 +12,15 @@ namespace ArslanElArabaları.DataAccessLayer.Concrete
 {
     public class TContext : IdentityDbContext<AdminUser, AdminRole, int>
     {
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=ArslanElArabalarıProjectDB;integrated security=true");
-        //    //optionsBuilder.UseSqlServer("server=DESKTOP-QMOGCM6; database=Test; integrated security=true;Encrypt=False;");
-        //    //"Data Source=DESKTOP-SM0VBLO;Initial Catalog =IsSureciDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-
-        //}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server= 77.245.159.27\\MSSQLSERVER2019;database=seyyarel_;user=enes1;password=1c2C3C4C5C*");
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=ArslanElArabalarıProjectDB;integrated security=true");
             //optionsBuilder.UseSqlServer("server=DESKTOP-QMOGCM6; database=Test; integrated security=true;Encrypt=False;");
             //"Data Source=DESKTOP-SM0VBLO;Initial Catalog =IsSureciDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
 
         }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("server=DESKTOP-4JQAL1L\\ENESSQLSERVER;database=ArslanElArabalarıProjectDB;integrated security=true");
-        //    //optionsBuilder.UseSqlServer("server=DESKTOP-QMOGCM6; database=Test; integrated security=true;Encrypt=False;");
-        //    //"Data Source=DESKTOP-SM0VBLO;Initial Catalog =IsSureciDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-
-        //}
+        
+       
         public DbSet<About> Abouts { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Experience> Experiences { get; set; }
